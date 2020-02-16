047bc5d5dd763c9b8b09e64166ed1ddc7cc81ecb
Thomas Ingram <code@ravinggenius.com>
2020-02-16 17:39:32 -0500

    List orders with status

--- src/pages/OrdersIndex.tsx
+++ src/pages/OrdersIndex.tsx
@@ -1,16 +1,47 @@
-import React from "react";
-import { Grid } from "semantic-ui-react";
+import React, { useContext } from "react";
+import { Grid, Table } from "semantic-ui-react";
 
-const P = "p";
+import PageTitle from "../components/layout/PageTitle";
+import RestaurantContext from "../contexts/RestaurantContext";
 
-const OrdersIndex: React.FunctionComponent = () => (
-	<Grid>
-		<Grid.Row>
-			<Grid.Column>
-				<P>such orders index</P>
-			</Grid.Column>
-		</Grid.Row>
-	</Grid>
-);
+const OrdersIndex: React.FunctionComponent = () => {
+	const {
+		getRecipe,
+		state: { orders }
+	} = useContext(RestaurantContext);
+
+	return (
+		<Grid>
+			<Grid.Row>
+				<Grid.Column>
+					<PageTitle>Orders</PageTitle>
+
+					<Table celled>
+						<Table.Header>
+							<Table.Row>
+								<Table.HeaderCell>
+									{"Recipe Name"}
+								</Table.HeaderCell>
+								<Table.HeaderCell>{"Status"}</Table.HeaderCell>
+							</Table.Row>
+						</Table.Header>
+
+						<Table.Body>
+							{orders.map(order => (
+								<Table.Row key={order.id}>
+									<Table.Cell>
+										{getRecipe(order.recipeId).name}
+									</Table.Cell>
+
+									<Table.Cell>{order.status}</Table.Cell>
+								</Table.Row>
+							))}
+						</Table.Body>
+					</Table>
+				</Grid.Column>
+			</Grid.Row>
+		</Grid>
+	);
+};
 
 export default OrdersIndex;
